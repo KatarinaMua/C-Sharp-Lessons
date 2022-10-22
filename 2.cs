4380dd59aeb991383ff88f6a8d86d6e25570f9b8
KatarinaMua <etikhomirova1808@gmail.com>
2022-10-22 22:34:10 +0300

    2 and 3 les

---
+++ 2.cs
@@ -0,0 +1,33 @@
+﻿/*
+Console.Write("Введи трёхзначное число: ");
+int _a = Convert.ToInt32(Console.ReadLine());
+string _s = Convert.ToString(_a);
+Console.WriteLine("Вторая цифра этого числа: "+_s[1]);
+*/
+
+/*
+Console.Write("Введи число: ");
+int _a = Convert.ToInt32(Console.ReadLine());
+string _aText = Convert.ToString(_a);
+if (_aText.Length > 2){
+  Console.WriteLine("третья цифра: " + _aText[2]);
+}
+else {
+  Console.WriteLine("Нет третьей цифры");
+}
+*/
+
+/*
+Console.Write("Введи номер дня недели: ");
+int _day = Convert.ToInt32(Console.ReadLine());
+void Weekend (int _day) {
+  if (_day == 6 || _day == 7) {
+  Console.WriteLine("это выходной");
+  }
+  else if (_day < 1 || _day > 7) {
+    Console.WriteLine("в неделе всего 7 дней");
+  }
+  else Console.WriteLine("это рабочий день");
+}
+Weekend(_day);
+*/
---
+++ 3.cs
@@ -0,0 +1,63 @@
+﻿/*
+// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+string? a = Console.ReadLine();
+void CheckCount(string a)
+{
+    if (a[0] == a[4] && a[1] == a[3])
+    {
+        Console.WriteLine($"Число {a} - палиндром.");
+    }
+    else Console.WriteLine($"Число {a} - не палиндром.");
+}
+if (a.Length == 5)
+{
+    CheckCount(a);
+}
+else Console.WriteLine($"Введите число из пяти цифр");
+*/
+
+
+
+/*
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int[] A = new int[3];
+int[] B = new int[3];
+Console.Write($"Введите координату X первой точки: ");
+int.TryParse(Console.ReadLine()!, out A[0]);
+Console.Write($"Введите координату Y первой точки: ");
+int.TryParse(Console.ReadLine()!, out A[1]);
+Console.Write($"Введите координату Z первой точки: ");
+int.TryParse(Console.ReadLine()!, out A[2]);
+Console.Write($"Введите координату X второй точки: ");
+int.TryParse(Console.ReadLine()!, out B[0]);
+Console.Write($"Введите координату Y второй точки: ");
+int.TryParse(Console.ReadLine()!, out B[1]);
+Console.Write($"Введите координату Z второй точки: ");
+int.TryParse(Console.ReadLine()!, out B[2]);
+int x = (A[0] - B[0]) * (A[0] - B[0]);
+int y = (A[1] - B[1]) * (A[1] - B[1]);
+int z = (A[2] - B[2]) * (A[2] - B[2]);
+double c = Math.Sqrt(x + y + z);
+c = Math.Round(c, 5);
+Console.Write($"Длина отрезка {c} пунктов");
+*/
+
+
+
+/*
+//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int a = 0;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+for (int i = 1; i <= a; i++)
+    Console.Write($"{i * i * i} ");
+*/
